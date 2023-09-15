c2436619034e86b88e045cedd8a4e0bb73407d6f
Jordan-p20 <jordan.p@live.ca>
2023-09-15 08:58:15 -0400

    nothing important

--- FPSTest/Assets/PlayerJoin.cs
+++ FPSTest/Assets/PlayerJoin.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerJoin : MonoBehaviour
 {
+    private PlayerInputManager inputManager;
+
+    private void Awake()
+    {
+        inputManager = GetComponent<PlayerInputManager>();
+    }
+
     public void OnPlayerJoin()
     {
-
+        if (inputManager.playerCount >= 2) { return; }
+        inputManager.JoinPlayer();
     }
 
     public void OnPLayerLeave()
